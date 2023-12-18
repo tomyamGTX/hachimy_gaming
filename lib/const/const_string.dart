b61e36c7c15438ce2037d727925b843b0bd1ed82
tomyamGTX <hakimimdnoor90@gmail.com>
2023-12-18 13:30:09 +0800

    optimize code

--- lib/const/const_string.dart
+++ lib/const/const_string.dart
@@ -1,8 +1,8 @@
-const appName = "Hachimy Gaming";
+const appName = "Kut Need Oreo Gaming";
 const tiktokUrl = "https://www.tiktok.com/@hakimimdnoor";
 const discordUrl = "https://discord.gg/ZJkeRhcKY8";
 const subscribeUrl = "https://vt.tiktok.com/ZSNawRhsn/";
-const charName = "Hutao";
-const supporterName = 'SUS Army';
-const charImagePath = 'assets/hutao.png';
+const charName = "Kokomi";
+const supporterName = 'Biskuties';
+const charImagePath = 'assets/kokomi.png';
 const bgImagePath = 'assets/bg.jpg';
